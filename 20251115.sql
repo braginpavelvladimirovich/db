34b7e4823e2ea003d0adc109a4648f20c115b9a4
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2025-11-15 14:46:15 +0700

    20251115

---
+++ 20251115.sql
@@ -0,0 +1,139 @@
+#Различные соединения таблиц
+#Выборка всех игр, которые находятся в разделе "Любимые" у нескольких пользователей
+SELECT 
+    game_name as 'Любимая игра'
+FROM
+    gamers a,
+    games,
+    gamers b
+WHERE
+    a.favorite_game = b.favorite_game
+        AND a.favorite_game = idgame and a.`name`<b.`name`;
+
+#Выборка всех игр, которые находятся в разделе "Любимые" у любых пользователей
+SELECT 
+    `games`.`game_name`, `gamers`.`name`
+FROM
+    `games`,
+    `gamers`
+WHERE
+    `gamers`.`favorite_game` = `games`.`idgame`
+ORDER BY game_name;
+
+#Оператор JOIN
+SELECT 
+    *
+FROM
+    gamers
+        JOIN
+    gamers_games USING (idgamers);
+#"Левое" и "правое" соединение
+SELECT 
+    *
+FROM
+    games
+        LEFT JOIN
+    gamers USING (country);
+    
+    SELECT DISTINCT
+    country
+FROM
+    games
+        RIGHT JOIN
+    gamers USING (country);
+#Внутреннее соединение
+SELECT DISTINCT
+    *
+FROM
+    games
+        INNER JOIN
+    gamers USING (country);
+SELECT DISTINCT
+    *
+FROM
+    games
+        CROSS JOIN
+    gamers USING (country);
+# Соединение JOIN при помощи ON   
+SELECT 
+    *
+FROM
+    games
+        JOIN
+    gamers_games ON idgame = game
+order by game_name;
+
+#Таблица с циклическими зависимостями. Соединение таблицы с собой
+SELECT 
+	t1.`сотрудник` as начальник, 'над', t2.`сотрудник` подчинённый 
+FROM 	20251025.`сотрудники` t1 
+		LEFT JOIN
+		20251025.`сотрудники` t2 ON t1.`ид_сотрудника` = t2.`номер_начальника`;
+SELECT 
+	t1.`сотрудник` as начальник, 'над', t2.`сотрудник` подчинённый 
+FROM 	20251025.`сотрудники` t1 
+		RIGHT JOIN
+		20251025.`сотрудники` t2 ON t1.`ид_сотрудника` = t2.`номер_начальника`;
+#"Естественное соединение" при помощи NATURAL JOIN
+SELECT 
+    `name`, `date`, `game_name`
+FROM
+    gamers
+        NATURAL JOIN
+    gamers_games
+        NATURAL JOIN
+    games;
+/*
+, `game_name`
+JOIN
+    games ON (gamers_games.idgame)
+*/    
+#При совпадении названий внешних ключей в связанных таблицах с первичными ключами можно использовать JOIN ON
+/* Нерабочий запрос 
+SELECT 
+    *
+FROM
+    gamers_games
+        JOIN
+    gamers using on (idgamers);
+*/
+
+SELECT 
+    *
+FROM
+    gamer
+        NATURAL JOIN
+    gamer_has_group
+        NATURAL JOIN
+    tank;
+SELECT 
+    *
+FROM
+    gamer
+        NATURAL JOIN
+    tank;
+SELECT 
+    *
+FROM
+    gamer
+        CROSS JOIN
+    tank;
+SELECT 
+    *
+FROM
+    gamer
+        JOIN
+    tank;
+SELECT 
+    *
+FROM
+    `20250930`.`gamer`
+        RIGHT JOIN
+    `20250930`.`gamer_has_group` on `gamer`.`group_grid` = `gamer_has_group`.`group_grid`;
+/* вывод игроков имеющих группу по ид группы */
+SELECT 
+    *
+FROM
+    `20250930`.`gamer`
+        LEFT JOIN
+    `20250930`.`gamer_has_group` ON `gamer`.`group_grid` = `gamer_has_group`.`group_grid`;
